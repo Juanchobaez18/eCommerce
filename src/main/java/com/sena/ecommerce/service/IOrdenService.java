c4996e72359de0b54ce44236594f8ce2c78f57e3
juanchobaez <juandavidbaezobregon@gmail.com>
2025-08-14 14:48:02 -0500

    v0.4_creacionInterface&clasesService_juanchobaez_14082025

--- src/main/java/com/sena/ecommerce/service/IOrdenService.java
+++ src/main/java/com/sena/ecommerce/service/IOrdenService.java
@@ -4,17 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import com.sena.ecommerce.model.Orden;
+import com.sena.ecommerce.model.Usuario;
 
 
 public interface IOrdenService {
 	
 	public Orden save(Orden orden);
 
-	public Optional<Orden> get(Integer id);
-	
-	public void update(Orden orden);
-	
-	public void delete(Integer id);
+	public Optional<Orden> findById(Integer id);
 	
 	public List<Orden> findAll();
+	
+	public List<Orden> findByUsuario(Usuario usuario);
+	
+	public String generarNumeroOrden();
 }
